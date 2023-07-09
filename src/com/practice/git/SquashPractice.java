dd6f53499c81c73ed054cc2f0d0bbfb9222e2d1f
dev_y <50270201+devy1540@users.noreply.github.com>
2023-07-09 20:40:04 +0900

    Squash 001 (#10)
    * squash-001 | 작업내역001
    
    * squash-001 | 작업내역002
    
    * squash-001 | 작업내역003
    
    ---------
    
    Co-authored-by: 플랫폼팀-윤혁준 <gurwns1540@ex-em.com>

---
+++ src/com/practice/git/SquashPractice.java
@@ -0,0 +1,15 @@
+package com.practice.git;
+
+public class SquashPractice {
+    public void squash001() {
+        System.out.println("001");
+    }
+
+    public void squash002() {
+        System.out.println("002");
+    }
+
+    public void squash003() {
+        System.out.println("003");
+    }
+}
