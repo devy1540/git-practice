dca057df4328bcbd3bf2d0e74ad0b00aa0abe1a9
플랫폼팀-윤혁준 <gurwns1540@ex-em.com>
2023-07-09 20:15:49 +0900

    rebase-001 | 작업내역

--- src/com/practice/git/RebasePractice.java
+++ src/com/practice/git/RebasePractice.java
@@ -1,7 +1,9 @@
 package com.practice.git;
 
 public class RebasePractice {
-
+    public void test001() {
+        System.out.println("test001");
+    }
     public void test002() {
         System.out.println("test002");
     }
